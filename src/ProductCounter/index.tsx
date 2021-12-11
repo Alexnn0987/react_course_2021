593588f72f788965c342609e37826ab60fb64c4c
Alexey Popov <alexnn0987@mail.ru>
2021-12-11 13:10:24 +0300

    after lesson_2

---
+++ src/ProductCounter/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+type ProductCounterPropsType = {
+  product: {
+    productName: string;
+    count?: number;
+  };
+};
+
+const ProductCounter: React.FC<ProductCounterPropsType> = (props) => {
+  const {
+    product: { productName, count = "Нет информации" },
+  } = props;
+  // const productName = props.productName;  //тоже самое
+
+  return (
+    <div className="product_counter">
+      <h1>{productName}</h1>
+      <span>{count}</span>
+    </div>
+  );
+};
+
+export default ProductCounter;
