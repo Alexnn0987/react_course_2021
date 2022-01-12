e78a465e3f0b2e8f9f7986c751b17db5a4dcfbc7
Alexey Popov <alexnn0987@mail.ru>
2022-01-12 23:20:03 +0300

    key_prop

---
+++ src/components/RegistrationComponent/index.tsx
@@ -0,0 +1,9 @@
+// import React from "react";
+
+// const RegistrationComponent () => {
+//     return(
+
+//     )
+// }
+
+// export default RegistrationComponent
---
+++ src/containers/RegistrationPageContainer/index.tsx
@@ -0,0 +1,9 @@
+// import React from "react";
+
+// const RegistrationPageContainer = () => {
+//     return(
+
+//     )
+// }
+
+// export default RegistrationPageContainer
